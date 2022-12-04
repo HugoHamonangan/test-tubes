4b5ed75140b83b26b27eb81f35be0ceae679fc77
hugo hamonangan <naberius103@gamil.com>
2022-12-04 17:58:00 +0700

    test

--- Home.py
+++ Home.py
@@ -4,6 +4,6 @@ st.title("SELAMAT DATANG DI OPERATOR MATEMATIKA")
 
 st.write("Aplikasi operator matematika sederhana yang memuat berbagai macam perhitungan")
 
-st.subheader("Kelompok : \n - Febriadi Lesamana / 3312201062  \n - Jessica Intan Kinasih / 3312201063 \n - Hugo Hamonangan / 3312201066 \n - Janrianto Rizki Pasaribu / 3312201077 \n - Sherina Nur Syabania / 3312201078 \n - Nurhadi / 3312201082")
+st.subheader("Kelompok : \n - Febriadi Lesmana / 3312201062  \n - Jessica Intan Kinasih / 3312201063 \n - Hugo Hamonangan / 3312201066 \n - Janrianto Rizki Pasaribu / 3312201077 \n - Sherina Nur Syabania / 3312201078 \n - Nurhadi / 3312201082")
 
 st.write("Untuk menggunakan aplikasi ini silahkan mengklik sidebar dengan icon panah di kiri atas.")
